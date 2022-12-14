e0801c6526d93a06af282eea366ad6f29ab56df2
rfadityas <raihanadit001@gmail.com>
2022-12-14 23:41:55 +0700

    Update TransaksiController.php

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -12,13 +12,13 @@ class TransaksiController extends Controller
     {
         $alltransactions = Transaction::all();
         $alltransactionstoday = Transaction::whereDate('created_at', Carbon::today())->orderby('created_at', 'desc')->get();
-        $revenuethismonth = Transaction::where('created_at', Carbon::now()->month)->sum('total_price');
+        $revenuethismonth = Transaction::whereMonth('created_at', Carbon::now()->month)->sum('total_price');
         $revenuelastmonth = Transaction::where(
             'created_at',
             '>=',
             Carbon::now()->subMonth()->toDateTimeString()
         )->sum('total_price');;
-        $revenuethisyear = Transaction::where('created_at', Carbon::now()->year)->sum('total_price');
+        $revenuethisyear = Transaction::whereYear('created_at', Carbon::now()->year)->sum('total_price');
         return view('transactions', [
             'alltransactions' => $alltransactions,
             'alltransactionstoday' => $alltransactionstoday,
